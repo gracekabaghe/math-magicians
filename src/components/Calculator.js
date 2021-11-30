1a37ff23ea952e872005fd79515278b9cd848aab
gracekabaghe <gracema2002@gmail.com>
2021-11-30 19:31:44 +0200

    Add components and ui

---
+++ src/components/Calculator.js
@@ -0,0 +1,30 @@
+import '../index.css';
+
+const Calculator = () => (
+  <div className="calculator-flex">
+    <div className="output" />
+    <div className="buttons">
+      <button type="button" className="button">AC</button>
+      <button type="button" className="button">+/-</button>
+      <button type="button" className="button">%</button>
+      <button type="button" className="button highlight">&divide;</button>
+      <button type="button" className="button">7</button>
+      <button type="button" className="button">8</button>
+      <button type="button" className="button">9</button>
+      <button type="button" className="button highlight">&times;</button>
+      <button type="button" className="button">4</button>
+      <button type="button" className="button">5</button>
+      <button type="button" className="button">6</button>
+      <button type="button" className="button highlight">-</button>
+      <button type="button" className="button">1</button>
+      <button type="button" className="button">2</button>
+      <button type="button" className="button">3</button>
+      <button type="button" className="button highlight">+</button>
+      <button type="button" className="button span-two">0</button>
+      <button type="button" className="button">.</button>
+      <button type="button" className="button highlight">=</button>
+    </div>
+  </div>
+);
+
+export default Calculator;
