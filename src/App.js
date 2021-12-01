2b32f65fbecb5b21cd8e1699657989428de92526
gracekabaghe <gracema2002@gmail.com>
2021-12-01 16:22:23 +0200

    Fix linting error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Calculator from './components/calculator';
+import Calculator from './Components/Calculator';
 
 class App extends Component {
   constructor(props) {
