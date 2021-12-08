4a3026b335d49f9d21cea00dc2e5308d3c248877
gracekabaghe <gracema2002@gmail.com>
2021-12-08 13:29:01 +0200

    fix linters

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -16,48 +16,47 @@ const Calculator = () => {
   };
   const { total, next, operation } = value;
 
- pages
-    const display = () => {
-      if (next) {
-        return next;
-      }
-      if (operation) {
-        return operation;
-      }
-      if (total) {
-        return total;
-      }
-      return 0;
-    };
-    return (
-      <div className="container">
-        <div className="cal-p">
-          <p>Solve some Maths</p>
-        </div>
-        <div className="calculator-flex">
-          <div className="output">{display()}</div>
-          <div className="buttons">
-            <Button value="AC" click={handler} />
-            <Button value="+/-" click={handler} />
-            <Button value="%" click={handler} />
-            <Button value="÷" click={handler} />
-            <Button value="7" click={handler} />
-            <Button value="8" click={handler} />
-            <Button value="9" click={handler} />
-            <Button value="x" click={handler} />
-            <Button value="4" click={handler} />
-            <Button value="5" click={handler} />
-            <Button value="6" click={handler} />
-            <Button value="-" click={handler} />
-            <Button value="1" click={handler} />
-            <Button value="2" click={handler} />
-            <Button value="3" click={handler} />
-            <Button value="-" click={handler} />
-            <Button value="+" click={handler} />
-            <Button value="0" click={handler} />
-            <Button value="." click={handler} />
-            <Button value="=" click={handler} />
-          </div>
+  // pages;
+  const display = () => {
+    if (next) {
+      return next;
+    }
+    if (operation) {
+      return operation;
+    }
+    if (total) {
+      return total;
+    }
+    return 0;
+  };
+  return (
+    <div className="container">
+      <div className="cal-p">
+        <p>Solve some Maths</p>
+      </div>
+      <div className="calculator-flex">
+        <div className="output">{display()}</div>
+        <div className="buttons">
+          <Button value="AC" click={handler} />
+          <Button value="+/-" click={handler} />
+          <Button value="%" click={handler} />
+          <Button value="÷" click={handler} />
+          <Button value="7" click={handler} />
+          <Button value="8" click={handler} />
+          <Button value="9" click={handler} />
+          <Button value="x" click={handler} />
+          <Button value="4" click={handler} />
+          <Button value="5" click={handler} />
+          <Button value="6" click={handler} />
+          <Button value="-" click={handler} />
+          <Button value="1" click={handler} />
+          <Button value="2" click={handler} />
+          <Button value="3" click={handler} />
+          <Button value="-" click={handler} />
+          <Button value="+" click={handler} />
+          <Button value="0" click={handler} />
+          <Button value="." click={handler} />
+          <Button value="=" click={handler} />
         </div>
       </div>
     </div>
